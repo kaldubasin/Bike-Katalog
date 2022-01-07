77263b75928440aa33507838e54f53779c92602d
kaldubasin <57008188+kaldubasin@users.noreply.github.com>
2022-01-07 18:48:44 +0300

    Initial Commit

---
+++ project 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project 5
+//
+//  Created by Alexey Bondarev on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
